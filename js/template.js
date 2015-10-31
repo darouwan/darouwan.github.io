5b81e63911d49a5df0771e9c566eb6ab8687d23c
Junfeng Chen <k-2feng@hotmail.com>
2015-10-31 09:56:55 +0800

    增加多开始画面，使用随机数切换

--- js/template.js
+++ js/template.js
@@ -10,7 +10,7 @@
 (function ($) {
     $(document).ready(function () {
 
-        $(".banner-image").backstretch('images/banner.jpg');
+        $(".banner-image").backstretch('images/banner' + parseInt(Math.random() * (7 + 1)) + '.jpg');
 
         // Fixed header
         //-----------------------------------------------
